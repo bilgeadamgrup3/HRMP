4124271fc26f18c21b5dd04aa72b3d4db0f4a7d2
Barış Mert Cömertoğlu <mertycom@gmail.com>
2023-01-31 11:02:58 +0300

    user-profile repository added

---
+++ userprofile-microservice/src/main/java/com/bilgeadam/repository/entity/UserProfile.java
@@ -0,0 +1,33 @@
+package com.bilgeadam.repository.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+@Table(name = "user_profiles")
+@Entity
+public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String middleName;
+    private String lastName;
+    private String lastName2;
+    private String profileImage;
+    private String identificationNumber;
+    private String birthplace;
+    private String birthDate;
+    private String job;
+    private String department;
+    private String email;
+    private String address;
+    private String telephone;
+}
